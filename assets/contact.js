6a0c4fe8d487719da2d59f6a139d84c6acbbdd49
jjhome123 <yee8217@pacificu.edu>
2021-02-13 15:35:47 +0900

    updates contact page

--- assets/contact.js
+++ assets/contact.js
@@ -1,4 +1,5 @@
 let white3 = document.getElementById('white3');
+let white4 = document.getElementById('white4');
 
 let white3Transparent = function() {
     white3.style.opacity = 0;
@@ -8,5 +9,15 @@ let white3Visible = function() {
     white3.style.opacity = 0.9;
 }
 
+let white4Transparent = function() {
+    white4.style.opacity = 0;
+}
+
+let white4Visible = function() {
+    white4.style.opacity = 0.9;
+}
+
 white3.onmouseenter = white3Transparent;
 white3.onmouseleave = white3Visible;
+white4.onmouseenter = white4Transparent;
+white4.onmouseleave = white4Visible;
